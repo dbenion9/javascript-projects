e8ee2957b6b01ce1f9bf7eee2430f9d89ac57e17
DeMarcus Benion <dbenion9@gmail.com>
2024-07-09 09:06:13 -0500

    Exercises: Unit Testing

--- unit-testing/exercises/RPS.js
+++ unit-testing/exercises/RPS.js
@@ -18,3 +18,18 @@ function whoWon(player1,player2){
  
    return 'Player 1 wins!';
  }
+ module.exports = {
+  whoWon: whoWon
+};
+
+const test = require('../RPS.js');
+
+test("returns 'Player 2 wins!' if P1 = rock & P2 = paper", function(){
+  let output = test.whoWon('rock','paper');
+  expect(output).toBe("Player 2 wins!");
+});
+
+test("returns 'Player 2 wins!' if P1 = paper & P2 = scissors", function(){
+  let output = test.whoWon('paper','scissors');
+  expect(output).toBe("Player 2 wins!");
+});
--- unit-testing/exercises/checkFive.js
+++ unit-testing/exercises/checkFive.js
@@ -9,3 +9,46 @@ function checkFive(num){
    }
    return result;
  }
+
+ const test = require('../checkFive.js');
+
+ const checkFive = require('../checkFive.js');
+
+ describe("checkFive", function(){
+
+    test("Descriptive feedback...", function() {
+       //code here...
+    });
+
+ });
+
+ const checkFive = require('../checkFive.js');
+
+ describe("checkFive", function(){
+
+    test("Descriptive feedback...", function(){
+       let output = checkFive(2);
+    });
+
+ });
+
+ const checkFive = require('../checkFive.js');
+
+ describe("checkFive", function(){
+
+    test("Descriptive feedback...", function(){
+       let output = checkFive(2);
+       expect(output).toEqual("2 is less than 5.");
+    });
+
+ });
+
+ test("returns 'num is less than 5' when num < 5.", function() {
+  // test code //
+});
+
+expect(output).toBe("2 is less than 5.");
+
+if (num > 5) {
+  // sourcecode //
+}
