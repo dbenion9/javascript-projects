a1a0b11766c26ff17c8759fc0df27e43b15f0f4d
DeMarcus Benion <dbenion9@gmail.com>
2024-08-05 13:04:08 -0500

    Dom and Events 1

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,28 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    // Task 1
+document.getElementById('liftoffButton').addEventListener('click', function() {
+    document.getElementById('status').innerText = 'Houston, we have liftoff!';
+});
+
+// Task 2
+document.getElementById('abortMission').addEventListener('mouseover', function() {
+    this.style.backgroundColor = 'red';
+});
+
+// Task 3
+document.getElementById('abortMission').addEventListener('mouseout', function() {
+    this.style.backgroundColor = '';
+});
+
+// Task 4
+document.getElementById('abortMission').addEventListener('click', function() {
+    if (confirm('Are you sure you want to abort the mission?')) {
+        document.getElementById('status').innerText = 'Mission aborted! Space shuttle returning home.';
+    }
+});
+
 }
 
 window.addEventListener("load", init);
